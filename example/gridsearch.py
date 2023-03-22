ccd1d02993d4c074ea932e497de438c2b86fa800
samanemami <saman.emami@hotmail.com>
2023-03-22 13:56:18 +0100

    Changes to be committed: 	new file:   example/gridsearch.py 	new file:   example/landmine_gbm.py 	new file:   example/landmine_xgboost.py 	new file:   example/school_gbm.py 	modified:   example/school_xgboost.py

---
+++ example/gridsearch.py
@@ -0,0 +1,59 @@
+from sklearn.model_selection import GridSearchCV, KFold
+from tqdm import tqdm
+import numpy as np
+import pandas as pd
+
+
+def gridsearch(X, 
+               y, 
+               estimator, 
+               classification, 
+               param_grid, 
+               random_state):
+
+    kfold = KFold(n_splits=3, 
+                  shuffle=True, 
+                  random_state=random_state)
+
+    if classification:
+        scoring = 'accuracy'
+    else:
+        scoring = 'neg_root_mean_squared_error'
+
+    pred = np.zeros_like(y)
+    bestparams = []
+    cv_results = []
+
+    for i, (train_index, test_index) in tqdm(enumerate(kfold.split(X, y))):
+        x_train, x_test = X[train_index], X[test_index]
+        y_train, y_test = y[train_index], y[test_index]
+
+        grid = GridSearchCV(estimator=estimator,
+                            param_grid=param_grid,
+                            scoring=scoring,
+                            n_jobs=-1,
+                            refit=True,
+                            cv=3)
+
+        grid.fit(x_train, y_train)
+        pred[test_index] = grid.predict(x_test)
+
+        bestparams.append(grid.best_params_)
+
+        grid.cv_results_[
+            'final_test_error'] = grid.score(x_test, y_test)
+
+    cv_results.append(grid.cv_results_)
+
+    results = {}
+    results['Metric'] = [scoring]
+    results['test_score'] = grid.cv_results_[
+        'mean_test_score'][grid.best_index_]
+    results['Mean_generalization_score'] = grid.cv_results_[
+        'final_test_error']
+
+    # pd.DataFrame(results).to_csv('_Summary.csv')
+    # pd.DataFrame(cv_results).to_csv('_CV_results.csv')
+    # pd.DataFrame(bestparams).to_csv('_Best_Parameters.csv')
+
+    return pred
--- example/school_gbm.py
+++ example/school_gbm.py
@@ -1,9 +1,8 @@
 import csv
 import numpy as np
 import pandas as pd
-from xgboost import XGBRegressor
+from sklearn.ensemble import GradientBoostingRegressor
 from sklearn.metrics import mean_squared_error
-from sklearn.preprocessing import LabelBinarizer
 from gridsearch import gridsearch
 
 
@@ -14,28 +13,24 @@ df = pd.merge(data, target)
 df = df.drop(columns=df.columns[0])
 
 
-scl = LabelBinarizer()
-
 tasks = np.unique(df.task)
 
-
-score = []
-pred = []
-
-param_grid = {'eta': [0.025, 0.05, 0.1, 0.5, 1],
+param_grid = {'learning_rate': [0.025, 0.05, 0.1, 0.5, 1],
               'max_depth': [2, 5, 10, 20],
               'subsample': [0.5, 0.75, 1]}
 
+score = []
+pred = []
 
 for task in tasks:
     data = df[df['task'] == task]
     X = (data.drop(columns=['task', '0'])).values
     y = (data['0']).values
 
-    model = XGBRegressor(eta=0.3,
-                            max_depth=6,
-                            subsample=1,
-                            n_estimators=100)
+    model = GradientBoostingRegressor(learning_rate=0.3,
+                                        max_depth=6,
+                                        subsample=1,
+                                        n_estimators=100)
 
     pred_ = gridsearch(X=X, y=y, 
                        estimator=model, 
@@ -46,9 +41,10 @@ for task in tasks:
     score.append(mean_squared_error(y, pred_, squared=False))
     pred.append(pred_)
 
-with open('xgboost_school_pred.csv', 'w', newline='') as csvfile:
+
+with open('GBM_school_pred.csv', 'w', newline='') as csvfile:
     writer = csv.writer(csvfile)
     for a in pred:
         writer.writerow(a)
 
-np.savetxt('xgboost_school_score.txt', score)
+np.savetxt('GBM_school_score.txt', score)
