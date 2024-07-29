660c742e5128c7f1f484038bc28a5038eff437d6
samanemami <saman.emami@hotmail.com>
2024-07-29 23:24:02 +0200

    Changes to be committed: 	modified:   Base/_Base.py 	modified:   model/mtgb.py

--- model/mtgb.py
+++ model/mtgb.py
@@ -6,64 +6,67 @@ from sklearn.tree._tree import DTYPE
 from sklearn.utils.validation import check_array
 import numpy as np
 
+
 class MTGBClassifier(GradientBoostingClassifier, BaseMTGB):
 
-    def __init__(self,
-                 *,
-                 loss='log_loss',
-                 learning_rate=0.1,
-                 n_estimators=100,
-                 subsample=1.0,
-                 criterion='squared_error',
-                 min_samples_split=2,
-                 min_samples_leaf=1,
-                 min_weight_fraction_leaf=0.,
-                 max_depth=3,
-                 min_impurity_decrease=0.,
-                 init=None,
-                 random_state=None,
-                 max_features=None,
-                 verbose=0,
-                 max_leaf_nodes=None,
-                 warm_start=False,
-                 validation_fraction=0.1,
-                 n_iter_no_change=None,
-                 tol=1e-4,
-                 ccp_alpha=0.0,
-                 n_common_estimators=0):
+    def __init__(
+        self,
+        *,
+        loss="log_loss",
+        learning_rate=0.1,
+        n_estimators=100,
+        subsample=1.0,
+        criterion="squared_error",
+        min_samples_split=2,
+        min_samples_leaf=1,
+        min_weight_fraction_leaf=0.0,
+        max_depth=3,
+        min_impurity_decrease=0.0,
+        init=None,
+        random_state=None,
+        max_features=None,
+        verbose=0,
+        max_leaf_nodes=None,
+        warm_start=False,
+        validation_fraction=0.1,
+        n_iter_no_change=None,
+        tol=1e-4,
+        ccp_alpha=0.0,
+        n_common_estimators=0
+    ):
 
-        super().__init__(loss=loss,
-                         learning_rate=learning_rate,
-                         n_estimators=n_estimators,
-                         criterion=criterion,
-                         min_samples_split=min_samples_split,
-                         min_samples_leaf=min_samples_leaf,
-                         min_weight_fraction_leaf=min_weight_fraction_leaf,
-                         max_depth=max_depth,
-                         init=init,
-                         subsample=subsample,
-                         max_features=max_features,
-                         random_state=random_state,
-                         verbose=verbose,
-                         max_leaf_nodes=max_leaf_nodes,
-                         min_impurity_decrease=min_impurity_decrease,
-                         warm_start=warm_start,
-                         validation_fraction=validation_fraction,
-                         n_iter_no_change=n_iter_no_change,
-                         tol=tol,
-                         ccp_alpha=ccp_alpha)
+        super().__init__(
+            loss=loss,
+            learning_rate=learning_rate,
+            n_estimators=n_estimators,
+            criterion=criterion,
+            min_samples_split=min_samples_split,
+            min_samples_leaf=min_samples_leaf,
+            min_weight_fraction_leaf=min_weight_fraction_leaf,
+            max_depth=max_depth,
+            init=init,
+            subsample=subsample,
+            max_features=max_features,
+            random_state=random_state,
+            verbose=verbose,
+            max_leaf_nodes=max_leaf_nodes,
+            min_impurity_decrease=min_impurity_decrease,
+            warm_start=warm_start,
+            validation_fraction=validation_fraction,
+            n_iter_no_change=n_iter_no_change,
+            tol=tol,
+            ccp_alpha=ccp_alpha,
+        )
         self.n_common_estimators = n_common_estimators
 
     def predict(self, X):
         raw_predictions = self.decision_function(X)
-        encoded_labels = \
-            self._loss._raw_prediction_to_decision(raw_predictions)
+        encoded_labels = self._loss._raw_prediction_to_decision(raw_predictions)
         return self.classes_.take(encoded_labels, axis=0)
 
     def staged_predict(self, X):
         for raw_predictions in self._staged_raw_predict(X):
-            encoded_labels = \
-                self._loss._raw_prediction_to_decision(raw_predictions)
+            encoded_labels = self._loss._raw_prediction_to_decision(raw_predictions)
             yield self.classes_.take(encoded_labels, axis=0)
 
     def decision_function(self, X, task_info=-1):
@@ -85,7 +88,7 @@ class MTGBClassifier(GradientBoostingClassifier, BaseMTGB):
             :term:`classes_`. Regression and binary classification produce an
             array of shape (n_samples,).
         """
-        X_full = X # not_necessary?
+        X_full = X  # not_necessary?
         X, t = self._split_task(X_full)
 
         X = self._validate_data(
@@ -99,55 +102,60 @@ class MTGBClassifier(GradientBoostingClassifier, BaseMTGB):
 
 class MTGBRegressor(GradientBoostingRegressor, BaseMTGB):
 
-    def __init__(self,
-                 *,
-                 loss='ls',
-                 learning_rate=0.1,
-                 n_estimators=100,
-                 subsample=1.0,
-                 criterion='squared_error',
-                 min_samples_split=2,
-                 min_samples_leaf=1,
-                 min_weight_fraction_leaf=0.,
-                 max_depth=3,
-                 min_impurity_decrease=0.,
-                 init=None,
-                 random_state=None,
-                 max_features=None,
-                 alpha=0.9,
-                 verbose=0,
-                 max_leaf_nodes=None,
-                 warm_start=False,
-                 validation_fraction=0.1,
-                 n_iter_no_change=None,
-                 tol=1e-4,
-                 ccp_alpha=0.0,
-                 metric='rmse'):
+    def __init__(
+        self,
+        *,
+        loss="ls",
+        learning_rate=0.1,
+        n_estimators=100,
+        subsample=1.0,
+        criterion="squared_error",
+        min_samples_split=2,
+        min_samples_leaf=1,
+        min_weight_fraction_leaf=0.0,
+        max_depth=3,
+        min_impurity_decrease=0.0,
+        init=None,
+        random_state=None,
+        max_features=None,
+        alpha=0.9,
+        verbose=0,
+        max_leaf_nodes=None,
+        warm_start=False,
+        validation_fraction=0.1,
+        n_iter_no_change=None,
+        tol=1e-4,
+        ccp_alpha=0.0,
+        n_common_estimators=0
+    ):
 
-        super().__init__(loss=loss,
-                         learning_rate=learning_rate,
-                         n_estimators=n_estimators,
-                         criterion=criterion,
-                         min_samples_split=min_samples_split,
-                         min_samples_leaf=min_samples_leaf,
-                         min_weight_fraction_leaf=min_weight_fraction_leaf,
-                         max_depth=max_depth,
-                         init=init,
-                         subsample=subsample,
-                         max_features=max_features,
-                         min_impurity_decrease=min_impurity_decrease,
-                         random_state=random_state,
-                         verbose=verbose,
-                         alpha=alpha,
-                         max_leaf_nodes=max_leaf_nodes,
-                         warm_start=warm_start,
-                         validation_fraction=validation_fraction,
-                         n_iter_no_change=n_iter_no_change,
-                         tol=tol,
-                         ccp_alpha=ccp_alpha)
+        super().__init__(
+            loss=loss,
+            learning_rate=learning_rate,
+            n_estimators=n_estimators,
+            criterion=criterion,
+            min_samples_split=min_samples_split,
+            min_samples_leaf=min_samples_leaf,
+            min_weight_fraction_leaf=min_weight_fraction_leaf,
+            max_depth=max_depth,
+            init=init,
+            subsample=subsample,
+            max_features=max_features,
+            min_impurity_decrease=min_impurity_decrease,
+            random_state=random_state,
+            verbose=verbose,
+            alpha=alpha,
+            max_leaf_nodes=max_leaf_nodes,
+            warm_start=warm_start,
+            validation_fraction=validation_fraction,
+            n_iter_no_change=n_iter_no_change,
+            tol=tol,
+            ccp_alpha=ccp_alpha,
+        )
+        self.n_common_estimators = n_common_estimators
 
     def predict(self, X):
-        X = check_array(X, dtype=DTYPE, order="C", accept_sparse='csr')
+        X = check_array(X, dtype=DTYPE, order="C", accept_sparse="csr")
         return self._raw_predict(X)
 
     def score(self, X, y):
